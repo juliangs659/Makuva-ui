4638a8d2795f78ade0588d4d0831a04f8ccc89a2
juliangs659 <julian_agutierrezs@soy.sena.edu.co>
2025-05-24 17:40:05 -0500

    Añadir iconos de FontAwesome a la barra de navegación y mejorar estilos responsivos

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,11 @@
 import './Nav.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faRoute, faMagnifyingGlassLocation, faCircleInfo, faBars} from '@fortawesome/free-solid-svg-icons'
 
 export default function Nav() {
   return (
     <nav>
+      <FontAwesomeIcon icon={faBars} className='nav-burguer' />
       <div className="nav-background">
         <a href="https://makuva-gold.vercel.app/" target='_blank'>
           <img
@@ -12,10 +15,10 @@ export default function Nav() {
         </a>
       </div>
       <ul className='nav-list'>
-        <li><a href="#home">Inicio</a></li>
-        <li><a href="#Routes">Mis Rutas</a></li>
-        <li><a href="#Explore">Explorar</a></li>
-        <li><a href="#about">Acerca de</a></li>
+        <li><a href="#home"><FontAwesomeIcon icon={faHouse} /> Inicio</a></li>
+        <li><a href="#Routes"><FontAwesomeIcon icon={faRoute} /> Mis Rutas</a></li>
+        <li><a href="#Explore"><FontAwesomeIcon icon={faMagnifyingGlassLocation} /> Explorar</a></li>
+        <li><a href="#about"><FontAwesomeIcon icon={faCircleInfo} /> Acerca de</a></li>
         <li><button type="Session">Iniciar Sesión</button></li>
       </ul>
     </nav>
